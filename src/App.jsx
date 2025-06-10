b45720b48617bacaa40a7013f03916c3b3bfe84c
Yeassin7376 <yeassinali17@gmail.com>
2025-06-10 10:38:32 +0600

    initial project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl'>B-11 Assignment-11 Library Management System</h1>
+    </>
+  )
+}
+
+export default App
