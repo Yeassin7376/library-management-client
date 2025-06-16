cfb7ef62f606a27d1e6a71088e50b22a8d28c932
Yeassin7376 <yeassinali17@gmail.com>
2025-06-17 03:03:52 +0600

    category books page completed

--- src/pages/categoryBooks/CategoryBooks.jsx
+++ src/pages/categoryBooks/CategoryBooks.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import { useLoaderData, useParams } from 'react-router';
+import { Link, useLoaderData, useParams } from 'react-router';
+import ReactStars from 'react-stars';
 
 const CategoryBooks = () => {
+  const { category } = useParams();
+  const data = useLoaderData();
+  console.log(category, data);
 
-    const {category} = useParams();
-    const data = useLoaderData();
-    console.log(category, data);
-    
-    const categoryBooks = data.filter(book => book.category?.trim().toLowerCase() === category.trim().toLowerCase());
-    console.log(categoryBooks);
-    
+  const categoryBooks = data.filter((book) => book.category?.trim().toLowerCase() === category.trim().toLowerCase());
+  console.log(categoryBooks);
 
-    return (
-        <div>
-            <h1 className='text-2xl md:text-5xl font-semibold'>Category Books For : {category}</h1>
+  return (
+    <div className="my-20">
+      <h1 className="text-2xl md:text-5xl font-semibold mb-5">Category Books For : {category}</h1>
 
-            <div>
-                {
-                    categoryBooks.map(book => <h2>{book.name}</h2>)
-                }
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 py-10">
+        {categoryBooks.map((book) => (
+          <div key={book._id} className="card bg-base-100 shadow-sm">
+            <figure>
+              <img className="h-[271px]" src={book.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {book.name}
+                <div className="badge badge-secondary">{book.category}</div>
+              </h2>
+              <p className='text-gray-400'>Author : {book.authorName}</p>
+              <p>
+                <ReactStars count={5} value={book.rating} edit={false} size={24} isHalf={true} activeColor="#ffd700" />
+              </p>
+              <p>Quantity : {book.quantity}</p>
+              <div className="card-actions justify-end">
+                <Link to={``}><button className="btn btn-primary">Details</button></Link>
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryBooks;
