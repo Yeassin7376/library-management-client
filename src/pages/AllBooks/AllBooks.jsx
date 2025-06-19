efcfe88a258c6b8d6cb3dc9d608a30c195aeadbd
Yeassin7376 <yeassinali17@gmail.com>
2025-06-19 12:39:23 +0600

    available book filter implement

--- src/pages/AllBooks/AllBooks.jsx
+++ src/pages/AllBooks/AllBooks.jsx
@@ -1,18 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import BookCard from './BookCard';
 
 const AllBooks = () => {
+  const initialBooks = useLoaderData();
   useEffect(() => {
     document.title = `All Books | Library`;
   }, []);
 
-  const books = useLoaderData();
-  // console.log(books);
+  const [isFiltered, setIsFiltered] = useState(false);
+const [books, setBooks] = useState(initialBooks);
+
+const availableBooks = initialBooks.filter(book => book.quantity !== 0);
+
+
+
+  const handleAvailableBooks = () => {
+    const toggle = !isFiltered
+    setIsFiltered(toggle);
+    console.log(isFiltered);
+
+    if (toggle) {
+      setBooks(availableBooks);
+    } else {
+      setBooks(initialBooks)
+    }
+    
+  };
 
   return (
     <div className="my-5 md:my-12">
-      <h1 className="text-2xl md:text-5xl font-bold mb-10 md:mb-20">All Books </h1>
+      <h1 className="text-2xl md:text-5xl font-bold mb-5 md:mb-6">All Books </h1>
+      <div className="w-full flex justify-end mb-5">
+        <button onClick={handleAvailableBooks} className={`btn bg-gray-100 ${isFiltered && 'bg-gray-300' }`}>
+          {
+            isFiltered ? 'All Books' : 'Show Available Books'
+          }
+          
+        </button>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {books.map((book) => (
           <BookCard key={book._id} book={book}></BookCard>
